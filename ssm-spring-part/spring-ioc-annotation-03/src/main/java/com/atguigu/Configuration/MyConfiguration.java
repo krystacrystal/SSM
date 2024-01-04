6fac4150c739d52dfd9763e6e7c2e290cba4dc79
chensisi <2532692867@qq.com>
2024-01-04 18:00:11 +0800

    modify

--- ssm-spring-part/spring-ioc-annotation-03/src/main/java/com/atguigu/Configuration/MyConfiguration.java
+++ ssm-spring-part/spring-ioc-annotation-03/src/main/java/com/atguigu/Configuration/MyConfiguration.java
@@ -1,8 +1,12 @@
 package com.atguigu.Configuration;
 
+import com.alibaba.druid.pool.DruidDataSource;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
+@PropertySource("classpath:jdbc.properties")
 @ComponentScan("com.atguigu")
 @Configuration
 public class MyConfiguration {
