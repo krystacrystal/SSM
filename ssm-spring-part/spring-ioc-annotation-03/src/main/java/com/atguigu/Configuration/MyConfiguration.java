de2926424109b391bdd03f977f9c13dcb5190c5d
chensisi <2532692867@qq.com>
2024-01-05 09:42:01 +0800

    modify

--- ssm-spring-part/spring-ioc-annotation-03/src/main/java/com/atguigu/Configuration/MyConfiguration.java
+++ ssm-spring-part/spring-ioc-annotation-03/src/main/java/com/atguigu/Configuration/MyConfiguration.java
@@ -1,15 +1,41 @@
 package com.atguigu.Configuration;
 
 import com.alibaba.druid.pool.DruidDataSource;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.jdbc.core.JdbcTemplate;
 
 @PropertySource("classpath:jdbc.properties")
 @ComponentScan("com.atguigu")
 @Configuration
 public class MyConfiguration {
+    @Value("${atguigu.url}")
+    private String url;
+    @Value("${atguigu.driver}")
+    private String driver;
+    @Value("${atguigu.username}")
+    private String username;
+    @Value("${atguigu.password}")
+    private String password;
 
+    @Bean(name = "ergouzi")
+    public DruidDataSource dataSource() {
+        DruidDataSource dataSource = new DruidDataSource();
+        dataSource.setUrl(url);
+        dataSource.setDriverClassName(driver);
+        dataSource.setUsername(username);
+        dataSource.setPassword(password);
+        return dataSource;
+    }
+
+    @Bean
+    public JdbcTemplate jdbcTemplate(DruidDataSource ergouzi) {
+        JdbcTemplate jdbcTemplate = new JdbcTemplate();
+        jdbcTemplate.setDataSource(ergouzi);
+        return jdbcTemplate;
+    }
 
 }
--- ssm-spring-part/spring-ioc-annotation-03/src/test/java/com/atguigu/test/AnnotationTest.java
+++ ssm-spring-part/spring-ioc-annotation-03/src/test/java/com/atguigu/test/AnnotationTest.java
@@ -1,5 +1,6 @@
 package com.atguigu.test;
 
+import com.alibaba.druid.pool.DruidDataSource;
 import com.atguigu.Configuration.MyConfiguration;
 import com.atguigu.components.CommonComponent;
 import com.atguigu.components.XxxController;
@@ -7,6 +8,7 @@ import com.atguigu.controller.SoldierController;
 import org.junit.jupiter.api.Test;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.jdbc.core.JdbcTemplate;
 
 public class AnnotationTest {
 
@@ -22,11 +24,19 @@ public class AnnotationTest {
     }
 
     @Test
-    public void test2(){
+    public void test2() {
         AnnotationConfigApplicationContext applicationContext
                 = new AnnotationConfigApplicationContext(MyConfiguration.class);
         SoldierController soldierController =
                 applicationContext.getBean(SoldierController.class);
         soldierController.getMessage();
+    }
+
+    @Test
+    public void test3() {
+        AnnotationConfigApplicationContext applicationContext
+                = new AnnotationConfigApplicationContext(MyConfiguration.class);
+        JdbcTemplate jdbcTemplate = applicationContext.getBean(JdbcTemplate.class);
+        System.out.println(jdbcTemplate);
     }
 }
